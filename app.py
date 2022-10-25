ffc580fa6b7c736867ec11ee4679e4751ff6c363
phwGithub <yey6689@gmail.com>
2022-10-25 19:39:08 +0900

    나의 일기 퍼블리싱

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template("index.html",title="정글 다이어리")
+   return render_template("monthlyDiary.html",title="정글 다이어리")
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
