d04080077769eb1893a83b09c4bd021d3e71bb53
phwGithub <yey6689@gmail.com>
2022-10-25 12:57:57 +0900

    commit test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return 'This is Home!!!'
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
