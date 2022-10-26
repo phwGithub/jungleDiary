dff0ef9052b5ee9b2596228ee6ad4a80f35fb43e
gcount85 <100mgml@gmail.com>
2022-10-26 14:17:27 +0900

    메인 페이지 토큰 검증 완료

--- app.py
+++ app.py
@@ -38,10 +38,13 @@ def validate_token(token):
 @app.route('/')
 def home():
     token = request.cookies.get('mytoken')
-    if validate_token(token) == '토큰만료' or '유효하지않은토큰':
+    print(token)
+    if (validate_token(token) == '토큰만료') or (validate_token(token) == '유효하지않은토큰'):
         return render_template("sign_in.html", title="정글 다이어리")
     else:
-        return render_template("monthlyDiary.html", title="정글 다이어리")
+        date = time.strftime('%Y %m %d %a', time.localtime(time.time()))
+        now_date = date.split(' ')
+        return render_template('main.html', title="오늘의 메모", date=now_date)
 
 
 # 회원가입 페이지로 이동
